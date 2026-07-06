540a0f31fb08bf6122968e2574266bf8d7a194e5
Nisar Ahmed <nisarahmed.webdev@gmail.com>
2026-07-06 13:34:20 +0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -40,7 +40,7 @@ INSERT INTO users (full_name, email, password, gender, country, role, privacy_ag
 VALUES (
     'Administrator',
     'admin@example.com',
-    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+    '$2y$12$nCUAUvAwTtrqlJBz65IxhunFEudrtShGqMpG07aav6Xvow1ZkV6OK',
     'male',
     'USA',
     'admin',
